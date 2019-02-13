03f77191f0f43398b9ae841f61b1c6e382c7560d
Kirill Assanovich <assanoooovi4k@gmail.com>
2019-02-13 03:54:38 +0300

    added ConnectionPool and ProxyPool

--- src/main/java/by/epam/java/training/assanoooovi4k/lifehacks/Main.java
+++ src/main/java/by/epam/java/training/assanoooovi4k/lifehacks/Main.java
@@ -1,13 +1,17 @@
 package by.epam.java.training.assanoooovi4k.lifehacks;
 
+import by.epam.java.training.assanoooovi4k.lifehacks.connection.ConnectorDB;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = ConnectorDB.getConnection()){
-            System.out.println("HI");
+//        try (Connection connection = ConnectorDB.getConnection()){
+//            System.out.println("HI");
+//
+//        }
 
-        }
+        ConnectorDB.
     }
 }
--- src/main/java/by/epam/java/training/assanoooovi4k/lifehacks/dao/UserDao.java
+++ src/main/java/by/epam/java/training/assanoooovi4k/lifehacks/dao/UserDao.java
@@ -3,16 +3,21 @@ package by.epam.java.training.assanoooovi4k.lifehacks.dao;
 import by.epam.java.training.assanoooovi4k.lifehacks.WrapperConnector;
 import by.epam.java.training.assanoooovi4k.lifehacks.model.entity.User;
 import by.epam.java.training.assanoooovi4k.lifehacks.model.entity.UserRole;
+import org.apache.log4j.Logger;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao extends AbstractDao<User>{
-    public static final String SQL_SELECT_ALL_USERS = "SELECT * FROM user";
+    private static final Logger LOGGER = Logger.getLogger(UserDao.class);
+
+    private static final String SQL_SELECT_ALL_USERS = "SELECT * FROM user";
+    private static final String SQL_SELECT_USERS_BY_ID = "SELECT * FROM user WHERE id = '%d'";
+    private static final String SQL_CREATE_USER = "INSERT INTO user(is_blocked, username, email, password, role) " +
+            "VALUES (false, ?, ?, ?, USER)";
 
     public UserDao() {
         this.connector = new WrapperConnector();
@@ -37,12 +42,33 @@ public class UserDao extends AbstractDao<User>{
 
     @Override
     public User findById(Long id) {
+        Statement statement = null;
+
         try {
-            PreparedStatement statement = (PreparedStatement) connector.getStatement();
+            statement = connector.getStatement();
+            ResultSet resultSet = statement.executeQuery(String.format(SQL_SELECT_USERS_BY_ID, id));
+            if (resultSet.next()) {
+                return createEntity(resultSet);
+            }
+            else {
+                LOGGER.info("No result set");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            this.closeStatement(statement);
         }
+
+        return null;
     }
+
+//    PreparedStatement preparedStatement = null;
+//    ResultSet resultSet = null;
+//        try {
+//        preparedStatement = connector.getPreparedStatement(SQL_SELECT_USERS_BY_ID);
+//    } catch (SQLException e) {
+//        e.printStackTrace();
+//    }
 
     @Override
     public boolean deleteById(Long id) {
@@ -55,8 +81,24 @@ public class UserDao extends AbstractDao<User>{
     }
 
     @Override
-    public boolean create(User entity) {
-        return false;
+    public boolean create(User user) {
+        boolean
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement= connector.getPreparedStatement(SQL_CREATE_USER);
+            preparedStatement.setString(2, user.getUsername());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getPassword());
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+        }
+
+        return null;
     }
 
     @Override
